718aa3749c6e3a71d60af8e83b05466c51756169
Thiago Yuiti <97604343+Yuit1@users.noreply.github.com>
2023-12-30 13:34:31 -0300

    Trocar senha de um usuário (AD).ps1
    Com regra padrão de segurança do AD Server.

---
+++ Trocar senha de um usuário (AD).ps1
@@ -0,0 +1,49 @@
+                # trocar senha
+                
+                function Get-RandomPassword {
+                    param (
+                        [int] $length = 8,
+                        [int] $upper = 1,
+                        [int] $lower = 1,
+                        [int] $numeric = 1,
+                        [int] $special = 1
+                    )
+                    if($upper + $lower + $numeric + $special -gt $length) {
+                        throw "number of upper/lower/numeric/special char must be lower or equal to length"
+                    }
+                    $uCharSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    $lCharSet = "abcdefghijklmnopqrstuvwxyz"
+                    $nCharSet = "0123456789"
+                    $sCharSet = "/*-+!?=()@$%&{}[]."
+                    $charSet = ""
+                    if($upper -gt 0) { $charSet += $uCharSet }
+                    if($lower -gt 0) { $charSet += $lCharSet }
+                    if($numeric -gt 0) { $charSet += $nCharSet }
+                    if($special -gt 0) { $charSet += $sCharSet }
+        
+                    $charSet = $charSet.ToCharArray()
+                    $rng = New-Object System.Security.Cryptography.RNGCryptoServiceProvider
+                    $bytes = New-Object byte[]($length)
+                    $rng.GetBytes($bytes)
+     
+                    $result = New-Object char[]($length)
+                    for ($i = 0 ; $i -lt $length ; $i++) {
+                        $result[$i] = $charSet[$bytes[$i] % $charSet.Length]
+                    }
+                    $password = (-join $result)
+                    $valid = $true
+                    if($upper   -gt ($password.ToCharArray() | Where-Object {$_ -cin $uCharSet.ToCharArray() }).Count) { $valid = $false }
+                    if($lower   -gt ($password.ToCharArray() | Where-Object {$_ -cin $lCharSet.ToCharArray() }).Count) { $valid = $false }
+                    if($numeric -gt ($password.ToCharArray() | Where-Object {$_ -cin $nCharSet.ToCharArray() }).Count) { $valid = $false }
+                    if($special -gt ($password.ToCharArray() | Where-Object {$_ -cin $sCharSet.ToCharArray() }).Count) { $valid = $false }
+     
+                    if(!$valid) {
+                        $password = Get-RandomPassword $length $upper $lower $numeric $special
+                    }
+                    return $password
+                }
+    
+                Get-RandomPassword 8
+
+                pause
+                cls
